b380bfb0c151f37eee43ee9d7689822cbff506df
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-05-30 11:39:12 +0200

    added java project

---
+++ org.softlang.maxmeffert.bscthesis.scenarios/src/org/softlang/maxmeffert/bscthesis/scenarios/model/Employee.java
@@ -0,0 +1,34 @@
+package org.softlang.maxmeffert.bscthesis.scenarios.model;
+
+public class Employee {
+
+	private String firstName;
+	private String familyName;
+	private int age;
+	private double salary;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getFamilyName() {
+		return familyName;
+	}
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}	
+}
