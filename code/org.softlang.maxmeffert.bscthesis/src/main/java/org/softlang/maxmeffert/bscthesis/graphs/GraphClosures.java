51c542f631efdd30d980194964216dd0e93048cf
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-10-02 20:19:21 +0200

    decoupled graph closures

--- code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/graphs/GraphClosures.java
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/graphs/GraphClosures.java
@@ -1,23 +1,26 @@
 package org.softlang.maxmeffert.bscthesis.graphs;
 
-import com.google.common.collect.Streams;
 import com.google.inject.Inject;
 import org.softlang.maxmeffert.bscthesis.utils.ICollectionFactory;
+import org.softlang.maxmeffert.bscthesis.utils.ICollectorFactory;
 import org.softlang.maxmeffert.bscthesis.utils.IPair;
-
-import java.util.stream.Collectors;
+import org.softlang.maxmeffert.bscthesis.utils.IStreamFactory;
 
 public class GraphClosures implements IGraphClosures {
 
     private final IGraphBuilders graphBuilders;
     private final IGraphWalkFactory graphWalkFactory;
     private final ICollectionFactory collectionFactory;
+    private final IStreamFactory streamFactory;
+    private final ICollectorFactory collectorFactory;
 
     @Inject
-    public GraphClosures(IGraphBuilders graphBuilders, IGraphWalkFactory graphWalkFactory, ICollectionFactory collectionFactory) {
+    public GraphClosures(IGraphBuilders graphBuilders, IGraphWalkFactory graphWalkFactory, ICollectionFactory collectionFactory, IStreamFactory streamFactory, ICollectorFactory collectorFactory) {
         this.graphBuilders = graphBuilders;
         this.graphWalkFactory = graphWalkFactory;
         this.collectionFactory = collectionFactory;
+        this.streamFactory = streamFactory;
+        this.collectorFactory = collectorFactory;
     }
 
     private <TValue extends Comparable<TValue>> IGraphBuilder<TValue> newGraphBuilder() {
@@ -31,9 +34,9 @@ public class GraphClosures implements IGraphClosures {
     }
 
     private <TValue extends Comparable<TValue>> Iterable<IPair<TValue,TValue>> buildReflexivePairs(IGraph<TValue> graph) {
-        return Streams.stream(graph.getNodes())
+        return streamFactory.newStream(graph.getNodes())
                 .map(node -> collectionFactory.newPair(node,node))
-                .collect(Collectors.toCollection(collectionFactory::newSortedSet));
+                .collect(collectorFactory.toSortedSet());
     }
 
     @Override
---
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/utils/CollectorFactory.java
@@ -0,0 +1,22 @@
+package org.softlang.maxmeffert.bscthesis.utils;
+
+import com.google.inject.Inject;
+
+import java.util.SortedSet;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class CollectorFactory implements ICollectorFactory {
+
+    private final ICollectionFactory collectionFactory;
+
+    @Inject
+    public CollectorFactory(ICollectionFactory collectionFactory) {
+        this.collectionFactory = collectionFactory;
+    }
+
+    @Override
+    public <TValue extends Comparable<TValue>> Collector<TValue, ?, SortedSet<TValue>> toSortedSet() {
+        return Collectors.toCollection(collectionFactory::<TValue>newSortedSet);
+    }
+}
