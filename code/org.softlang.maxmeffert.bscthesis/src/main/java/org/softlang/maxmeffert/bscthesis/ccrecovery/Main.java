23d26c77d5c790104f396cf5e6c86fc472ef0658
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-10-15 14:08:12 +0200

    added old scenario files

--- code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/Main.java
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/Main.java
@@ -5,16 +5,21 @@ import org.softlang.maxmeffert.bscthesis.ccrecovery.core.ccrecovery.ICCRecovery;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.core.correspondences.ICorrespondenceDefinition;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.core.languages.ILanguage;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.core.parsers.ParserException;
+
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.java8.Java8Lexer;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.java8.Java8Parser;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.model.Company;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.xml.XMLLexer;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.xml.XMLParser;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, ParserException {
+	public static void main(String[] args) throws IOException, ParserException, JAXBException {
 		ICCRecovery ccRecovery = CCRecovery.create();
 
 		ILanguage java8 = ccRecovery.defineLanguage("Java8", Java8Lexer::new, Java8Parser::new, Java8Parser::compilationUnit);
@@ -26,7 +31,15 @@ public class Main {
 			return true;
 		});
 
-		ccRecovery.findCorrespondences(correspondenceDefinition,"class A {class A {}}","class A {class A {}}");
+//		ccRecovery.findCorrespondences(correspondenceDefinition,"class A {class A {}}","class A {class A {}}");
+        JAXBContext jaxbContext = JAXBContext.newInstance(Company.class);
+        Marshaller m = jaxbContext.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        Company company = new Company();
+        company.setName("softlang llc");
+        // Write to System.out
+        m.marshal(company, System.out);
  	}
 
 }
