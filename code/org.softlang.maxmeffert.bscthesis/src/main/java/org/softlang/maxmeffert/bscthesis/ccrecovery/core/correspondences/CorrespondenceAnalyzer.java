e5f1d7bf9f13ed7f1388bfeb62ad66e95580cc98
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-10-17 18:29:07 +0200

    added new correspondence analyzer signature

--- code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/core/correspondences/CorrespondenceAnalyzer.java
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/core/correspondences/CorrespondenceAnalyzer.java
@@ -1,10 +1,12 @@
 package org.softlang.maxmeffert.bscthesis.ccrecovery.core.correspondences;
 
-import org.softlang.maxmeffert.bscthesis.ccrecovery.core.fragments.IFragment;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.core.fragmentkbs.IFragmentKB;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.core.fragmentkbs.IFragmentKBFactory;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.core.fragments.IFragment;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.core.languages.ILanguage;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.core.parsers.IParserResult;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.core.similarities.ISimilarity;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.core.utils.views.IView;
 
 import java.io.InputStream;
 import java.util.Set;
@@ -42,6 +44,26 @@ public class CorrespondenceAnalyzer implements ICorrespondenceAnalyzer {
             for (IFragment fragment2 : fragmentKB2.getFragments()) {
                 if (correspondenceDefinition.accept(fragment1, fragment2)) {
                     correspondences.add(correspondenceFactory.newCorrespondence(correspondenceDefinition, fragment1, fragment2));
+                }
+            }
+        }
+        return correspondences;
+    }
+
+    @Override
+    public Set<ICorrespondence> analyze(ISimilarity similarity, IFragment fragment1, IFragment fragment2) {
+        Set<ICorrespondence> correspondences = new TreeSet<>();
+        IFragmentKB fragmentKB1 = fragmentKBFactory.newFragmentKB(fragment1);
+        IFragmentKB fragmentKB2 = fragmentKBFactory.newFragmentKB(fragment2);
+        for (IFragment f1 : fragmentKB1.getFragments()) {
+            for (IFragment f2 : fragmentKB2.getFragments()) {
+                if (similarity.contains(f1, f2)) {
+                    IView<IFragment> fragmentsOfF1 = fragmentKB1.getFragmentsOf(f1);
+                    IView<IFragment> fragmentsOfF2 = fragmentKB2.getFragmentsOf(f2);
+                    boolean correspondsTo = fragmentsOfF1.all( ff1 -> fragmentsOfF2.any( ff2 -> similarity.contains(ff1,ff2)));
+                    if (correspondsTo) {
+                        // add correspondence
+                    }
                 }
             }
         }
--- code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/core/correspondences/ICorrespondenceAnalyzer.java
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/core/correspondences/ICorrespondenceAnalyzer.java
@@ -1,11 +1,13 @@
 package org.softlang.maxmeffert.bscthesis.ccrecovery.core.correspondences;
 
-import org.softlang.maxmeffert.bscthesis.ccrecovery.core.parsers.ParserException;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.core.fragments.IFragment;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.core.similarities.ISimilarity;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Set;
 
 public interface ICorrespondenceAnalyzer {
+    @Deprecated
     Set<ICorrespondence> analyze(ICorrespondenceDefinition correspondenceDefinition, InputStream artifact1, InputStream artifact2) throws Exception;
+    Set<ICorrespondence> analyze(ISimilarity similarity, IFragment fragment1, IFragment fragment2);
 }
