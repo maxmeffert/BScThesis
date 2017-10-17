26caba277340b1055081e90974abefa4bb1cc2cb
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-10-17 15:12:30 +0200

    refactored xml fragment ast factory

--- code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/scenarios/xml/fast/XMLFragmentBuildingListener.java
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/scenarios/xml/fast/XMLFragmentBuildingListener.java
@@ -1,13 +1,10 @@
 package org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.xml.fast;
 
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.misc.Interval;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.core.fragments.IFragment;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.core.fragments.IFragmentBuildingListener;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.xml.antlr.XMLParser;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.xml.antlr.XMLParserBaseListener;
 
-import java.util.List;
 import java.util.Stack;
 
 public class XMLFragmentBuildingListener extends XMLParserBaseListener implements IFragmentBuildingListener {
@@ -17,43 +14,14 @@ public class XMLFragmentBuildingListener extends XMLParserBaseListener implement
     private Stack<XMLElementFragment> xmlElementFragments = new Stack<>();
     private Stack<XMLDocumentFragment> xmlDocumentFragments = new Stack<>();
 
-    private <T> List<T> popAllInto(Stack<T> stack, List<T> list) {
-        while(!stack.isEmpty()) {
-            list.add(stack.pop());
-        }
-        return list;
-    }
-
-    private String textof(ParserRuleContext parserRuleContext) {
-        int a = parserRuleContext.start.getStartIndex();
-        int b = parserRuleContext.stop.getStopIndex();
-        return parserRuleContext.getStart().getInputStream().getText(Interval.of(a,b));
-    }
-
     @Override
     public void exitElement(XMLParser.ElementContext ctx) {
-        super.exitElement(ctx);
-        String name = ctx.Name(0).getText();
-        String text = textof(ctx);
-        XMLElementFragment xmlElementFragment = xmlFragmentFactory.newXMLElementFragment();
-        xmlElementFragment.setName(ctx.Name(0).toString());
-        xmlElementFragment.setText(textof(ctx));
-        while (!xmlElementFragments.isEmpty()) {
-            xmlElementFragment.addElement(xmlElementFragments.pop());
-        }
-        xmlElementFragments.push(xmlElementFragment);
+        xmlElementFragments.push(xmlFragmentFactory.newXMLElementFragment(ctx, xmlElementFragments));
     }
 
     @Override
     public void exitDocument(XMLParser.DocumentContext ctx) {
-        super.exitDocument(ctx);
-        String text = textof(ctx);
-        XMLDocumentFragment xmlDocumentFragment = xmlFragmentFactory.newXMLDocumentFragment();
-        xmlDocumentFragment.setText(textof(ctx));
-        while(!xmlElementFragments.isEmpty()) {
-            xmlDocumentFragment.addElement(xmlElementFragments.pop());
-        }
-        xmlDocumentFragments.push(xmlDocumentFragment);
+        xmlDocumentFragments.push(xmlFragmentFactory.newXMLDocumentFragment(ctx, xmlElementFragments));
     }
 
     @Override
--- code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/scenarios/xml/fast/XMLFragmentFactory.java
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/scenarios/xml/fast/XMLFragmentFactory.java
@@ -1,12 +1,35 @@
 package org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.xml.fast;
 
-public class XMLFragmentFactory {
+import org.softlang.maxmeffert.bscthesis.ccrecovery.core.fragments.BaseFragmentFactory;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.xml.antlr.XMLParser;
 
-    public XMLDocumentFragment newXMLDocumentFragment() {
-        return new XMLDocumentFragment();
+import java.util.Stack;
+
+public class XMLFragmentFactory extends BaseFragmentFactory {
+
+    public XMLDocumentFragment newXMLDocumentFragment(XMLParser.DocumentContext documentContext) {
+        XMLDocumentFragment xmlDocumentFragment = initialize(new XMLDocumentFragment(), documentContext);
+        return xmlDocumentFragment;
     }
 
-    public XMLElementFragment newXMLElementFragment() {
-        return new XMLElementFragment();
+    public XMLDocumentFragment newXMLDocumentFragment(XMLParser.DocumentContext documentContext, Stack<XMLElementFragment> xmlElementFragments) {
+        XMLDocumentFragment xmlDocumentFragment = newXMLDocumentFragment(documentContext);
+        while (!xmlElementFragments.isEmpty()) {
+            xmlDocumentFragment.addElement(xmlElementFragments.pop());
+        }
+        return xmlDocumentFragment;
+    }
+
+    public XMLElementFragment newXMLElementFragment(XMLParser.ElementContext elementContext) {
+        XMLElementFragment xmlElementFragment = initialize(new XMLElementFragment(), elementContext);
+        xmlElementFragment.setName(elementContext.Name(0).getText());
+        return xmlElementFragment;
+    }
+    public XMLElementFragment newXMLElementFragment(XMLParser.ElementContext elementContext, Stack<XMLElementFragment> xmlElementFragments) {
+        XMLElementFragment xmlElementFragment = newXMLElementFragment(elementContext);
+        while (!xmlElementFragments.isEmpty()) {
+            xmlElementFragment.addElement(xmlElementFragments.pop());
+        }
+        return xmlElementFragment;
     }
 }
