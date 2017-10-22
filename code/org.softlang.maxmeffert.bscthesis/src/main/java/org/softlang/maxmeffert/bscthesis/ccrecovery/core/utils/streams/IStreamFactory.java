e47f1606e31ac99755fa6fde299e9bf1db897fce
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-10-22 17:33:08 +0200

    cleanup removed unused stream factories

--- code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/core/utils/streams/IStreamFactory.java
+++
@@ -1,7 +0,0 @@
-package org.softlang.maxmeffert.bscthesis.ccrecovery.core.utils.streams;
-
-import java.util.stream.Stream;
-
-public interface IStreamFactory {
-    <TValue> Stream<TValue> newStream(Iterable<TValue> iterable);
-}
