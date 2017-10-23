095593074225856382aa24457472073280107883
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-10-23 20:13:40 +0200

    cleanup: moved reusable functions into java string utils

--- code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/scenarios/technologies/jaxb/JaxbXmlCorrespondenceNamingSimilarityHeuristic.java
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/scenarios/technologies/jaxb/JaxbXmlCorrespondenceNamingSimilarityHeuristic.java
@@ -3,56 +3,51 @@ package org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.technologies.jaxb
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.languages.java.fragmentast.*;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.languages.xml.fragmentast.XMLAttributeFragmentAST;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.languages.xml.fragmentast.XMLElementFragmentAST;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.utils.JavaStringUtils;
 import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.utils.StringUtils;
 
 public class JaxbXmlCorrespondenceNamingSimilarityHeuristic extends BaseJaxbSimilarityHeuristic {
 
-    private static final String[] JavaAccessorPrefixes = new String[] {"get", "set", "is"};
-
-    private String removeAnyJavaAccessorPrefix(String string) {
-        return StringUtils.removeAnyPrefix(string, JavaAccessorPrefixes);
-    }
-
-    private boolean areSimilar(String javaIdentifier, String xmlName) {
+    private boolean hasJaxbNamingSimilarities(String javaIdentifier, String xmlName) {
         return StringUtils.areEqual(javaIdentifier,xmlName)
                 || StringUtils.areLowerCaseEqual(javaIdentifier,xmlName)
                 || StringUtils.areLowerCaseEqual(StringUtils.removePluralSuffix(javaIdentifier), xmlName)
-                || StringUtils.areLowerCaseEqual(removeAnyJavaAccessorPrefix(javaIdentifier), xmlName)
-                || StringUtils.areLowerCaseEqual(removeAnyJavaAccessorPrefix(StringUtils.removePluralSuffix(javaIdentifier)), xmlName);
+                || StringUtils.areLowerCaseEqual(JavaStringUtils.removeAnyJavaAccessorPrefix(javaIdentifier), xmlName)
+                || StringUtils.areLowerCaseEqual(JavaStringUtils.removeAnyJavaAccessorPrefix(StringUtils.removePluralSuffix(javaIdentifier)), xmlName);
     }
 
     @Override
     protected boolean similar(JavaClassFragmentAST javaClassFragment, XMLElementFragmentAST xmlElementFragment) {
         String identifier = javaClassFragment.getIdentifier();
         String name = xmlElementFragment.getName();
-        return areSimilar(identifier, name);
+        return hasJaxbNamingSimilarities(identifier, name);
     }
 
     @Override
     protected boolean similar(JavaFieldFragmentAST javaFieldFragment, XMLElementFragmentAST xmlElementFragment) {
         String identifier = javaFieldFragment.getIdentifier();
         String name = xmlElementFragment.getName();
-        return areSimilar(identifier, name);
+        return hasJaxbNamingSimilarities(identifier, name);
     }
 
     @Override
     protected boolean similar(JavaMethodFragmentAST javaMethodFragment, XMLElementFragmentAST xmlElementFragment) {
         String identifier = javaMethodFragment.getIdentifier();
         String name = xmlElementFragment.getName();
-        return areSimilar(identifier, name);
+        return hasJaxbNamingSimilarities(identifier, name);
     }
 
     @Override
     protected boolean similar(JavaFieldFragmentAST javaFieldFragment, XMLAttributeFragmentAST xmlAttributeFragment) {
         String identifier = javaFieldFragment.getIdentifier();
         String name = xmlAttributeFragment.getName();
-        return areSimilar(identifier, name);
+        return hasJaxbNamingSimilarities(identifier, name);
     }
 
     @Override
     protected boolean similar(JavaMethodFragmentAST javaMethodFragment, XMLAttributeFragmentAST xmlAttributeFragment) {
         String identifier = javaMethodFragment.getIdentifier();
         String name = xmlAttributeFragment.getName();
-        return areSimilar(identifier, name);
+        return hasJaxbNamingSimilarities(identifier, name);
     }
 }
---
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/scenarios/utils/JavaStringUtils.java
@@ -0,0 +1,11 @@
+package org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.utils;
+
+public class JavaStringUtils {
+
+    private static final String[] JavaAccessorPrefixes = new String[] {"get", "set", "is"};
+
+    public static String removeAnyJavaAccessorPrefix(String string) {
+        return StringUtils.removeAnyPrefix(string, JavaAccessorPrefixes);
+    }
+
+}
