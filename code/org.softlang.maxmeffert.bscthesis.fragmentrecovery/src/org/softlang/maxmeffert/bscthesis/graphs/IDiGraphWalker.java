03f5dd3893b6d254dd5a2cb5669ac5c9d207c315
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-11-02 11:12:42 +0100

    removed old recovery project

--- code/org.softlang.maxmeffert.bscthesis.fragmentrecovery/src/org/softlang/maxmeffert/bscthesis/graphs/IDiGraphWalker.java
+++
@@ -1,8 +0,0 @@
-package org.softlang.maxmeffert.bscthesis.graphs;
-
-import java.util.function.Consumer;
-
-public interface IDiGraphWalker<T extends Comparable<T>> {
-    void walkDepthFirst(IDiGraph<T> diGraph, T start, Consumer<T> consumer);
-    void walkBackDepthFirst(IDiGraph<T> diGraph, T start, Consumer<T> consumer);
-}
