093a54b3cd84c1da6a9fad5b4b904ea129243784
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-11-04 16:07:21 +0100

    started annotation based heurisitc

--- code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/scenarios/technologies/heuristics/hibernate/HibernateJavaSqlAnnotationCorrespondenceSimilarityHeuristic.java
+++ code/org.softlang.maxmeffert.bscthesis/src/main/java/org/softlang/maxmeffert/bscthesis/ccrecovery/scenarios/technologies/heuristics/hibernate/HibernateJavaSqlAnnotationCorrespondenceSimilarityHeuristic.java
@@ -1,4 +1,25 @@
 package org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.technologies.heuristics.hibernate;
 
-public class HibernateJavaSqlAnnotationCorrespondenceSimilarityHeuristic {
+import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.languages.java.fragments.JavaClassFragment;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.languages.java.fragments.JavaFieldFragment;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.languages.java.fragments.JavaMethodFragment;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.languages.sql.fragments.SqlColumnFragment;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.languages.sql.fragments.SqlCreateTableFragment;
+import org.softlang.maxmeffert.bscthesis.ccrecovery.scenarios.technologies.heuristics.BaseJavaSqlSimilarityHeuristic;
+
+public class HibernateJavaSqlAnnotationCorrespondenceSimilarityHeuristic extends BaseJavaSqlSimilarityHeuristic {
+    @Override
+    protected boolean areSimilar(JavaClassFragment javaClassFragment, SqlCreateTableFragment sqlCreateTableFragment) {
+        return false;
+    }
+
+    @Override
+    protected boolean areSimilar(JavaFieldFragment javaFieldFragment, SqlColumnFragment sqlColumnFragment) {
+        return false;
+    }
+
+    @Override
+    protected boolean areSimilar(JavaMethodFragment javaMethodFragment, SqlColumnFragment sqlColumnFragment) {
+        return false;
+    }
 }
