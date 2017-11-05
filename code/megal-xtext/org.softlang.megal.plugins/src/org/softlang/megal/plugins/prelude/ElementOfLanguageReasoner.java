c03b0aa00d6403a47bf3e4d040d638213157c458
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-11-05 19:28:16 +0100

    added reasoner to resolver elementOf relations with superset languages

---
+++ code/megal-xtext/org.softlang.megal.plugins/src/org/softlang/megal/plugins/prelude/ElementOfLanguageReasoner.java
@@ -0,0 +1,31 @@
+package org.softlang.megal.plugins.prelude;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.softlang.megal.mi2.Entity;
+import org.softlang.megal.mi2.KB;
+import org.softlang.megal.mi2.Relationship;
+import org.softlang.megal.plugins.api.GuidedReasonerPlugin;
+
+public class ElementOfLanguageReasoner extends GuidedReasonerPlugin {
+	
+	private Collection<Entity> getSuperLanguagesOf(KB kb, Entity language) {
+		return kb.getRelationships().stream()
+				.filter(rel -> rel.getTypeName().equals("subsetOf") )
+				.filter(rel -> rel.getLeft().getName().equals(language.getName()))
+				.map(rel -> rel.getRight())
+				.collect(Collectors.toList());
+	}
+	
+	protected void guidedDerive(Relationship relationship) throws Throwable {
+		KB kb = relationship.getKB();
+		this.when(relationship.getTypeName().equals("elementOf"));
+		this.when(relationship.getRight().isInstance(kb.getEntityType("Language")));
+				
+		for(Entity superLang : getSuperLanguagesOf(kb, relationship.getRight())) {
+			this.relationship(relationship.getLeft().getName(), superLang.getName(), "elementOf");
+		}
+		
+	}
+}
