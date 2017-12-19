ad0e3e349de77cc98a284e0e718df4a5e09109ef
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-12-19 23:01:15 +0100

    cleanup: removed folder old, moved addendum one level up

--- thesis/old/code/quicksort.hs
+++
@@ -1,8 +0,0 @@
-module Quicksort (
-   quicksort
-) where
-
-   quicksort [] = []
-   quicksort [x] = [x] 
-   quicksort (x:xs) = (quicksort [a|a <- xs, a <= x]) ++ [x] ++ (quicksort [a| a <- xs, a > x]) 
-   
